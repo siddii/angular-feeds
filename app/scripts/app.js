88997764b8a38a2d655862dc8ec1e17bdb813256
Siddique Hameed <siddii+git@gmail.com>
2014-01-16 09:03:00 -0600

    Generated from yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('angularFeedsApp', [
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularFeedsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
