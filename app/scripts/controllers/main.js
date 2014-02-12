61b8e7f283c275c2cb28ddd346708bb0419a839d
siddique <siddii@gmail.com>
2014-02-11 18:16:46 -0600

    markup toggle

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,8 +2,17 @@
 
 angular.module('angular-feeds-demo')
   .controller('FeedController', function ($scope, $element) {
+    $scope.markup = false;
 
     $scope.toggleMarkup = function () {
-      $element.find('feed ul').fadeToggle();
+      var fadeElements = ['feed ul', 'pre'];
+
+      $element.find(!$scope.markup ? fadeElements[0] : fadeElements[1]).fadeOut(function (){
+        $element.find(!$scope.markup ? fadeElements[1] : fadeElements[0]).fadeIn(function (){
+          $scope.$apply(function (){
+            $scope.markup = !$scope.markup;
+          });
+        });
+      });
     };
   });
