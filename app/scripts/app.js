9bb7702f99728257f59acc9e97b56ef2f83673b8
siddique <siddii@gmail.com>
2014-02-11 19:15:05 -0600

    markup toggle

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,13 +4,4 @@ angular.module('angular-feeds-demo', [
   'feeds',
   'ngSanitize',
   'ngRoute'
-]).directive('feedMarkup', function(){
-  return {
-    restrict: 'M',
-    link: function(scope, elem, attr) {
-      var commentText = elem[0].textContent;
-      var feedMarkup = commentText.substr(commentText.indexOf('feedMarkup') + 11);
-      elem.parent().parent().find('.fa-code').popover({trigger: 'click', placement:'left', content: feedMarkup});
-    }
-  };
-});
+]);
