444d9cc7dfdee0ce53251040012692471ce48a8f
siddique <siddii@gmail.com>
2014-02-12 14:05:01 -0600

    scrolling to example

--- app/scripts/docs.js
+++ app/scripts/docs.js
@@ -4,7 +4,18 @@ $(function (){
     window.prettyPrint();
   }, 2000); //Yuck!
 
+  $('.navbar-nav').find('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 50
+        }, 1000);
+      }
+    }
+  });
 });
 
 
