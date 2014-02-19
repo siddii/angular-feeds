e97a6bfaff382cd2ad03106609612b54dde77b4f
siddique <siddii@gmail.com>
2014-02-19 08:48:49 -0600

    file refactoring

---
+++ app/scripts/feed-tester.js
@@ -0,0 +1,17 @@
+angular.module('angular-feeds-tester', [
+    'feeds'
+  ]).controller('FeedTesterController', ['$scope', '$compile', function ($scope, $compile) {
+    $scope.count = 10;
+    $scope.summary = false;
+    $scope.showFeed = function (){
+      var $panelBody = $('.panel-body');
+      $panelBody.html('');
+      var feedHTML = "<feed url='" + $scope.url + "' count='" + $scope.count + "' summary='" + $scope.summary + "'/>";
+      $panelBody.append($compile(feedHTML)($scope));
+    };
+
+    var url = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
+    $scope.isOrigin = function (){
+      return window.parent.location.href.indexOf(url) === -1;
+    };
+  }]);
---
+++ app/scripts/my-feeds.js
@@ -0,0 +1,26 @@
+angular.module('angular-my-feeds', [
+    'feeds'
+  ]).controller('MyFeedsController', ['$scope', '$compile', '$http', '$rootScope', function ($scope, $compile, $http, $rootScope) {
+    $scope.isOrigin = function () {
+      var url = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
+      return window.parent.location.href.indexOf(url) !== -1;
+    };
+
+    $http.get('my-feeds.json').success(function (feeds) {
+      $scope.feeds = feeds;
+    });
+  }])
+  .directive('feedWidget', ['$compile', function ($compile) {
+    return  {
+      restrict: 'A',
+      controller: ['$scope', '$element', '$attrs', '$timeout', function ($scope, $element) {
+        var feed = $scope.feed;
+        var feedHTML = "<feed url='" + feed.url + "' count='" + feed.count + "' post-render='feedPostRender()'/>";
+        $element.append($compile(feedHTML)($scope));
+      }]
+    };
+  }]);
+
+function feedPostRender($element) {
+  console.log('##### $element = ', $(this));
+}
