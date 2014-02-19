347ad81a114707b884970b7f64a849aedc7da941
Siddique Hameed <siddii+git@gmail.com>
2014-02-19 16:14:29 -0600

    feed summary

--- app/scripts/my-feeds.js
+++ app/scripts/my-feeds.js
@@ -16,7 +16,8 @@ angular.module('my-feeds', [
       restrict: 'A',
       controller: ['$scope', '$element', '$attrs', '$timeout', function ($scope, $element) {
         var feed = $scope.feed;
-        var feedHTML = "<feed url='" + feed.url + "' count='" + feed.count + "' post-render='feedPostRender'/>";
+        console.log('##### feed.summary = ', feed.summary);
+        var feedHTML = "<feed url='" + feed.url + "' count='" + feed.count + "' summary=" + (feed.summary ? 'true' : 'false') + " post-render='feedPostRender'/>";
         $element.append($compile(feedHTML)($scope));
       }]
     };
