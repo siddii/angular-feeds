0b9c5f4a91ebe2c5b377f89b5702ef72d625bb37
Siddique Hameed <siddii+git@gmail.com>
2014-02-19 19:55:34 -0600

    persisting feed in localstorage

--- app/scripts/my-feeds.js
+++ app/scripts/my-feeds.js
@@ -1,15 +1,27 @@
 angular.module('my-feeds', [
     'feeds'
   ])
-  .controller('MyFeedsController', ['$scope', '$compile', '$http', '$rootScope', function ($scope, $compile, $http, $rootScope) {
+  .controller('MyFeedsController', ['$scope', '$compile', '$http', 'storage', function ($scope, $compile, $http, storage) {
     $scope.isOrigin = function () {
       var url = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
       return window.parent.location.href.indexOf(url) !== -1;
     };
 
-    $http.get('my-feeds.json').success(function (feeds) {
-      $scope.feeds = feeds;
-    });
+    $scope.$watch('feeds', function (newValue, oldValue) {
+      if (newValue) {
+        storage.set('feeds', newValue);
+      }
+    }, true);
+
+    if (!storage.get('feeds')) {
+      $http.get('my-feeds.json').success(function (feeds) {
+        $scope.feeds = feeds;
+      });
+    }
+    else {
+      $scope.feeds = storage.get('feeds');
+    }
+
   }])
   .directive('feedWidget', ['$compile', function ($compile) {
     return  {
@@ -26,12 +38,22 @@ angular.module('my-feeds', [
       $scope.collapsed = !$scope.collapsed;
     };
 
-    $scope.deleteFeed = function (){
-      if (confirm('Are you sure you want to delete this feed?') &&  $scope.feeds.indexOf($scope.feed) > -1) {
+    $scope.deleteFeed = function () {
+      if (confirm('Are you sure you want to delete this feed?') && $scope.feeds.indexOf($scope.feed) > -1) {
         $scope.feeds.splice($scope.feeds.indexOf($scope.feed), 1);
       }
     }
-  }]);
+  }])
+  .factory('storage', function () {
+    return {
+      set: function (name, obj) {
+        localStorage[name] = angular.toJson(obj);
+      },
+      get: function (name) {
+        return angular.fromJson(localStorage[name]);
+      }
+    };
+  });
 
 function feedPostRender(element) {
   $(element).find('a').attr('target', '_blank');
